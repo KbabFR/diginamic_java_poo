25fb8bfaec639f357c59c050588ad1db6ccbb9b9
KbabFR <43203741+KbabFR@users.noreply.github.com>
2022-06-09 09:35:23 +0200

    ajout TP 4

---
+++ src/utils/ConversionNombre.java
@@ -0,0 +1,17 @@
+
+
+package utils;
+
+public class ConversionNombre {
+
+	public static void main(String[] args) {
+		
+		String chaine = "12";
+		int a = Integer.parseInt(chaine);
+		int b = 15;
+		System.out.println(Integer.max(a, b));
+		
+
+	}
+
+}
