4a40a44ee936d9d5cfa5c23c969cd4933cab6e87
KbabFR <43203741+KbabFR@users.noreply.github.com>
2022-06-14 14:59:55 +0200

    Ajout TP15

---
+++ src/fichier/ManipulationFichier.java
@@ -0,0 +1,33 @@
+package fichier;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ManipulationFichier {
+
+	public static void main(String[] args) throws IOException {
+		Path path = Paths.get("C:\\Users\\totow\\Documents\\workspace_spring\\ensemble\\donnees_communes.csv");
+		
+		Path pathDestination = Paths.get("C:\\Users\\totow\\Documents\\workspace_spring\\ensemble\\donnees_communes_25000_habitants_minimum.csv");
+		
+		List<String> fichier = new ArrayList<>();
+		
+		List<String> lines = Files.readAllLines(path);
+		for (String line : lines) {
+			String[] data = line.split(";");
+			if(Integer.parseInt(data[9]) > 25000){
+				String ville = new String(data[6] + ";" + data[0] + ";" + data[9]);
+				fichier.add(ville);
+			}
+		}
+		Files.write(pathDestination, fichier);
+
+	}
+
+}
