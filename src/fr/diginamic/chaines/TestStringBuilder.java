6a31042159ea9d2a295da3d26f9cf23447090d20
KbabFR <43203741+KbabFR@users.noreply.github.com>
2022-06-14 16:45:01 +0200

    Ajout TP16

--- src/fr/diginamic/chaines/TestStringBuilder.java
+++ src/fr/diginamic/chaines/TestStringBuilder.java
@@ -3,7 +3,30 @@ package fr.diginamic.chaines;
 public class TestStringBuilder {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		long debut = System.currentTimeMillis();
+		
+		StringBuilder build = new StringBuilder();
+		
+		for (int i = 1; i < 100001; i++) {
+			build.append(i);
+		}
+		
+		long fin = System.currentTimeMillis();
+		
+		System.out.println(String.format("Temps écoulé en milisecondes en utilisant le StringBuilder : %s \n", fin - debut));
+		
+		debut = System.currentTimeMillis();
+		
+		String str = new String();
+		
+		for (int i = 1; i < 100001; i++) {
+			str += i;
+		}
+		
+		fin = System.currentTimeMillis();
+		
+		System.out.println(String.format("Temps écoulé en milisecondes en utilisant la concaténation \"+\" : %s", fin - debut));
 
 	}
 
